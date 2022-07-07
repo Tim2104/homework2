afbd9207bcdbb62611afc4e5bbfc856522a0b59f
Tim2104 <rostov-r89@mail.ru>
2022-07-07 18:38:16 +0300

    ...

---
+++ index.js
@@ -0,0 +1,38 @@
+
+const express = require('express');
+let app = express();
+
+let connect = [];
+let count = 0;
+
+app.get('/', (req, res, next) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.setHeader('Transfer-Encoding', 'chunked');
+    connect.push(res);
+})
+.listen(3000, () => {
+    console.log('start');
+});
+
+
+setTimeout(function timer() {
+    console.log(`conter: ${count} ; now: ${new Date().toUTCString()}`);
+
+    count++;
+
+    if(count > 30) {
+        for(let i = 0; i < connect.length; i++) {
+            connect[i].write(`stop  (${new Date().toUTCString()})`);
+            connect[i].end();
+        }
+        //connect = [];
+        //count = 0;
+        process.exit();
+    }
+
+    for(let i = 0; i < connect.length; i++) {
+        connect[i].write(`id: ${i} <br>`);
+    }
+
+    setTimeout(timer, 1000);
+}, 1000);
